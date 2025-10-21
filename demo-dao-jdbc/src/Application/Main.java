b80d33d6d3c6d6bd521f85a7215b33cde998572a
Tun1n <slpedro21@outlook.com>
2025-10-21 08:27:46 -0300

    Department entity class

---
+++ demo-dao-jdbc/src/Application/Main.java
@@ -0,0 +1,12 @@
+package Application;
+
+
+import model.entities.Department;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "um");
+        System.out.println(obj);
+    }
+}
