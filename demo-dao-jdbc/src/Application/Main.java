6131a3e8a27effe0c7a6f88be1a23cdde374a099
Tun1n <slpedro21@outlook.com>
2025-10-21 11:06:24 -0300

    Delete implementation

--- demo-dao-jdbc/src/Application/Main.java
+++ demo-dao-jdbc/src/Application/Main.java
@@ -7,9 +7,13 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -33,7 +37,6 @@ public class Main {
         for(Seller obj: list){
             System.out.println(obj);
         }
-
         System.out.println();
 
         System.out.println("===== Test 4: seller Insert =====");
@@ -41,11 +44,22 @@ public class Main {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
+        System.out.println();
+
         System.out.println("===== Test 5: seller Update =====");
         seller = sellerDao.findById(1);
         seller.setName("Marta wayne");
         sellerDao.update(seller);
         System.out.println("Update completed! ");
+
+        System.out.println();
+
+        System.out.println("===== Test 6: seller Delete =====");
+        System.out.println("Enter Id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed! ");
+        sc.close();
 
     }
 }
