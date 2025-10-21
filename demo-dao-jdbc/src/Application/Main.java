b146d082db926a58ef40b025c501a3b149637145
Tun1n <slpedro21@outlook.com>
2025-10-21 08:33:09 -0300

    Seller entity class

--- demo-dao-jdbc/src/Application/Main.java
+++ demo-dao-jdbc/src/Application/Main.java
@@ -2,11 +2,17 @@ package Application;
 
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "um");
         System.out.println(obj);
+
+        Seller sel = new Seller(21, "Bob","bob@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(sel);
     }
 }
